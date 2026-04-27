4fbe0e74d284eed3d884b3cb8d0f3d5a9bc4e998
Garuba Ridwanullahi Omobolaji <152794319+Bolar001@users.noreply.github.com>
2026-04-27 15:47:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="MEE 316 · Filter Lab", page_icon="🎛️", layout="wide")
+st.set_page_config(page_title="EEE 316 · Filter Lab", page_icon="🎛️", layout="wide")
 
 # ── Custom CSS ────────────────────────────────────────────────────────────────
 st.markdown("""
